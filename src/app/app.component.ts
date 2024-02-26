35ca18b43ea106ace96c83791205fad6cee0a356
kalp-bhadane <kalpeshbhadane77@gmail.com>
2024-02-27 00:53:14 +0530

    adding bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-playground';
+  isAuthenticated = true;
+  constructor(
+    // private authService: AuthService
+    ) {}
+
+  ngOnInit() {
+    // this.authService.userSub.subscribe((user) => {
+    //   this.isAuthenticated = user ? true : false;
+    // });
+  }
+
+  onLogout(event: Event) {
+    event.preventDefault();
+    // this.authService.logout();
+  }
 }
