92c1327eafcef244dd15f009ea5a81ad10ad7dc7
kalp-bhadane <kalpeshbhadane77@gmail.com>
2024-03-03 22:15:58 +0530

    adding compoenent to test pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
